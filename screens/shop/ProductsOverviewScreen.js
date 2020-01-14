db77265809d3f1d43d9ee13dc84cd88793f8d7cc
Ralence <ranko.ostojic@gmail.com>
2020-01-14 21:30:28 +0100

    feat: add data model and basic redux set up

---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+
+const ProductsOverviewScreen = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    );
+};
+
+export default ProductsOverviewScreen;
